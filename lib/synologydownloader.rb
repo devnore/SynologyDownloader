a7a2f9933dca06b644230aec9085237e8b7f8235
Simon Ingelsson <simon@ispy.se>
2014-06-08 12:17:52 +0200

    Move-files

--- lib/synologydownloader.rb
+++ lib/synologydownloader.rb
@@ -13,21 +13,16 @@ require_relative 'piratesearch'
 
 # Main Class
 class SynologyDownloader
-  attr_reader :database_file , :state_db, :downloader, :settings_dir
-  attr_writer :downloader
+  attr_reader :database_file , :state_db, :downloader, :settings_dir, :dl
+  attr_writer :downloader, :dl
 
   def initialize(database_file = nil, settings_file = nil) # rubocop:disable MethodLength
     @settings_dir =  File.expand_path('~/.SynologyDownloader/')
-    @temp_dir = File.expand_path("#{@settings_dir}/temp/")
     FileUtils.mkdir_p(File.dirname(@settings_dir))
-    FileUtils.mkdir_p(File.dirname(@temp_dir))
-
     database_file ||= File.expand_path('~/.SynologyDownloader/database.yml')
-
     @database_file = database_file
     settings_file ||= File.expand_path('~/.SynologyDownloader/settings.yml')
     @settings_file = settings_file
-
     @settings = load_yml(@settings_file)
     @state_db = load_yml(@database_file)
     @now = DateTime.now.strftime('%Y-%m-%d')
@@ -35,9 +30,16 @@ class SynologyDownloader
 
   # Run this app
   def run
-    move_start
-    load_rss
-    save_db if fetcher.download_start
+    @dl = Synology::DSM.new(@settings['downloader'])
+    if @dl.login
+      move_start
+      load_rss
+      save_db if download_start
+    else
+      puts 'No connection to Server.'
+    end
+  ensure
+    @dl.logout
   end
 
   def load_yml(file)
@@ -85,57 +87,43 @@ class SynologyDownloader
   end
 
   def download_start
-    fail('wrong args.') unless @settings['downloader']
-    dl = Synology::DSM.new(@settings['downloader'])
-    if dl.login
-      add_downloads(dl)
-    else
-      puts 'No connection to Server.'
-    end
-    dl.logout
+    add_downloads
   end
 
   def move_start
-    dl = Synology::DSM.new(@settings['downloader'])
-    if dl.login
-      move_process_list(dl, JSON.parse(dl.list(@settings['downloader']['base_dir'])), true, true)
-    else
-      puts 'No connection to Server.'
-    end
-    dl.logout
+    path = getshare(@settings['shares']['download'])
+    move_process_list(JSON.parse(@dl.list(path)), true, true)
   end
 
-  def move_process_list(dl, list, recursive = false, parent_is_root = true)
+  def move_process_list(list, recursive = false, parent_is_root = true)
     list['data']['files'].each do |e|
       if e['isdir'] && recursive
-        move_process_list(dl, JSON.parse(dl.list(e['path'])) , false, false)
+        move_process_list(JSON.parse(@dl.list(e['path'])) , false, false)
       end
       unless @settings['file']['type']['movie'].include?(e['additional']['type'])
         next
       end
       info = ToName.to_name(e['name'])
-      if info.series.nil? && info.episode.nil?
-        puts dl.mkdir(@settings['downloader']['movies'], 'Incomming/')
-        if parent_is_root
-          file_to_move = e['path']
-        else
-          file_to_move = File.dirname(e['path'])
-        end
-        puts dl.move(file_to_move, "#{@settings['downloader']['movies']}/Incomming/" , true)
-      else
-        dl.mkdir(@settings['downloader']['series'], "#{info.name}/Season #{info.series.to_s.rjust(2, '0')}/")
-        puts dl.move(e['path'], "#{@settings['downloader']['series']}/#{info.name}/Season #{info.series.to_s.rjust(2, '0')}/", true)
-      end
-    end
-  end
 
-  def to_s
-    'A database of Downloaded stuff...'
+      type = info.series.nil? ? 'movies' : 'series'
+      path_base = getshare(@settings['shares'][type])
+
+      case type
+      when 'movies'
+        path = ''
+        file_to_move = parent_is_root ? e['path'] : File.dirname(e['path'])
+      when 'series'
+        path = "#{info.name.gsub(/\w+/, &:capitalize)}/Season #{info.series.to_s.rjust(2, '0')}/"
+        dl.mkdir(path_base, path)
+        file_to_move = e['path']
+      end
+      dl.move(file_to_move, "#{path_base}/#{path}" , true)
+    end
   end
 
   private
 
-  def add_downloads(dl)
+  def add_downloads
     @state_db.each do |key, item|
       item['done'] = dl.download(item['url']) unless item['done']
     end
@@ -143,5 +131,10 @@ class SynologyDownloader
 
   def merge_recursively(a, b)
     a.merge(b) { |key, a_item, b_item| merge_recursively(a_item, b_item) }
+  end
+
+  def getshare(share, create = true)
+    data = dl.mkdir(share['share'], share['path'].gsub(/^[\/]+/, '')) if create
+    "#{share['share']}#{share['path']}"
   end
 end
